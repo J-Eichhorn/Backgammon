c52667d9470cd0087c41c7375210f6d3dbae5860
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-11 14:24:35 -0400

    begin to build out the board

--- lib/board.py
+++ lib/board.py
@@ -5,4 +5,46 @@ from player import Player
 class Board:
 
     def __init__(self):
-        pass
+        self.columns = []
+        self.columns_ascii = []
+
+        self.create_columns()
+        self.create_board()
+    
+    def create_columns(self):
+        for i in range(24):
+            self.columns.append(Column(i + 1))
+
+    def create_board(self):
+        # board = '''
+        # ========================================================================
+        # |  {}     {}     {}     {}     {}     {}     {}     {}     {}    {}    {}    {} |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # |                                                                      |
+        # | {}    {}    {}    {}    {}    {}    {}    {}    {}    {}    {}    {} |
+        # ========================================================================
+
+
+        # '''.format(*[column.number for column in self.columns], sep = ",")
+        # print(board)
+
+        for x in range(12):
+            self.columns_ascii.append([self.columns[x].number, '*', '*', '*', '*', '*', '','*', '*', '*', '*', '*', self.columns[23-x].number])
+
+            
+            for piece in self.columns[x].num_pieces:
+                self.columns_ascii[piece]
+            self.columns[23-x].num_pieces
+
+        self.columns_ascii.reverse()
+        for row in range(13):
+            print('{:^6} {:^6} {:^6} {:^6} {:^6} {:^6} |    | {:^6} {:^6} {:^6} {:^6} {:^6} {:^6}'.format(*[column[row] for column in self.columns_ascii], sep = ","))
--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,6 @@
 import ipdb
 from player import Player
+from board import Board
 
 if __name__ == '__main__':
     pass
