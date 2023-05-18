5f8b6d90d2877c259cddd706452b4f3e5340d6e0
Racquel Glickman <racquel.glickman@gmail.com>
2023-05-18 15:56:32 -0400

    shortened some code

--- lib/move.py
+++ lib/move.py
@@ -23,13 +23,12 @@ class Move:
             self.turns = 4
 
     def user_move(self):
-        # add doubles behavior
         
         turn_count = 0
         turn_over = False
-        while turn_count < self.turns:
+        while turn_count < self.turns: # this is the main loop that tells it how many turns to do
 
-            if self.turns == 4:    
+            if self.turns == 4: # script for doubles
                 if len(self.dice) == 4:
                     time.sleep(1)
                     print(f"Your turn, {self.player.name} ({self.player.color.title()})!")
@@ -45,12 +44,11 @@ class Move:
                     time.sleep(1)
                     print(f'You rolled doubles, {self.player.name}! You have {len(self.dice)} turns left!')
                 elif len(self.dice) == 1:
-                    print(self.dice)
                     time.sleep(1)
                     print(f'One more turn, {self.player.name} ({self.player.color.title()})!')
                     time.sleep(1)
                     print(f'Your remaining roll: ({self.dice[0]})')
-            elif self.turns == 2:
+            elif self.turns == 2: # script for a regular turn
                 if len(self.dice) == 2:
                     time.sleep(1)
                     print(f"Your turn, {self.player.name} ({self.player.color.title()})!")
@@ -65,39 +63,33 @@ class Move:
             valid_origin = False
             valid_destination = False
 
-            valid_input1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24',]
+            valid_input1 = [str(x) for x in [*range(1,25)]]
 
-            valid_input2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', 'off']
+            valid_input2 = [str(x) for x in [*range(1,25)]]
+            valid_input2.append('off')
 
-            if self.player.color not in Move.jail:
-                if self.check_available_moves() == False:
+            # am i allowed to choose an origin?
+            if self.player.color not in Move.jail: # player is not in jail
+                if self.check_available_moves() == False: # but no available moves
                     time.sleep(1)
                     print(f'Sorry {self.player.name}, no available moves!')
                     time.sleep(2)
-                    turn_over = True
+                    turn_count = self.turns
+                    self.recreate_board()
+                    break
                 else:
                     time.sleep(1)
-                    origin_column = input('Which column would you like to move from? ')
-                    while origin_column not in valid_input1:
-                        origin_column = input('Please enter valid origin column: ')                  
-            else:
+                    origin_column = self.get_column_number('origin', valid_input1, True)                  
+            else: # player is in jail
                 if self.player.color == 'red':
                     origin_column = 0
                 elif self.player.color == 'white':
                     origin_column = 25
+            possible_destinations = self.generate_possible_destinations(origin_column)
 
-            if turn_over == True:
-                turn_count = self.turns
-                print('you found me')
-                self.clear()
-                self.board.columns_ascii = []
-                self.board.create_board()
-                break
-
-            possible_destinations = self.generate_possible_destinations(int(origin_column))
-
+            # once i can choose an origin, is it valid?
             while valid_origin == False:
-                if self.player.color in Move.jail:
+                if self.player.color in Move.jail: # player in jail
                     if self.check_possible_destinations(possible_destinations):
                         Move.jail.remove(self.player.color)
                         valid_origin = True
@@ -107,37 +99,29 @@ class Move:
                         time.sleep(2)
                         turn_over = True
                         break
-                elif self.board.columns[int(origin_column) - 1].color_status == self.player.color:
+                elif self.board.columns[origin_column - 1].color_status == self.player.color: # player not in jail, valid origin
                     if self.check_possible_destinations(possible_destinations):
                         valid_origin = True
                     else:
-                        origin_column = input('Please enter valid origin column: ')
-                        while origin_column not in valid_input1:
-                            origin_column = input('Please enter valid origin column: ')  
-                        possible_destinations = self.generate_possible_destinations(int(origin_column))
-                else:
-                    origin_column = input('Please enter valid origin column: ')
-                    while origin_column not in valid_input1:
-                        origin_column = input('Please enter valid origin column: ')  
-                    possible_destinations = self.generate_possible_destinations(int(origin_column))
+                        origin_column = self.get_column_number('origin', valid_input1) 
+                        possible_destinations = self.generate_possible_destinations(origin_column)
+                else: # player not in jail, invalid origin
+                    origin_column = self.get_column_number('origin', valid_input1) 
+                    possible_destinations = self.generate_possible_destinations(origin_column)
 
             if turn_over == True:
                 turn_count = self.turns
-                self.clear()
-                self.board.columns_ascii = []
-                self.board.create_board()
+                self.recreate_board()
                 break
             else:
-                destination_column = input('Which column would you like to move to? ')
-                while destination_column not in valid_input2:
-                    destination_column = input('Please enter valid destination column: ')
+                destination_column = self.get_column_number('destination', valid_input2, True) 
 
-            
+            # once i have a valid origin, is the destination that i choose valid?
             while valid_destination == False:
 
-                if destination_column == 'off':
+                if destination_column == 'off': # if you try to take piece 'off'
                     if self.allowed_off():
-                        possible_destinations = self.generate_possible_destinations(int(origin_column))
+                        possible_destinations = self.generate_possible_destinations(origin_column)
                         if len(self.dice) >= 2:
                             if (possible_destinations[0] > 24 or possible_destinations[0] < 1) or (possible_destinations[1] > 24 or possible_destinations[1] < 1):
                                 dice_input = input('Which die roll do you want to use? ')
@@ -164,26 +148,23 @@ class Move:
                                 self.maneuver_pieces(origin_column)
                                 valid_destination = True
                     else:
-                        destination_column = input('Please enter valid destination column: ')
-                        while destination_column not in valid_input2:
-                            destination_column = input('Please enter valid destination column: ')
+                        destination_column = self.get_column_number('destination', valid_input2) 
 
-                elif abs(int(destination_column) - int(origin_column)) in self.dice:
-
-                    if int(destination_column) - int(origin_column) > 0:
+                elif abs(destination_column - origin_column) in self.dice: # if you enter a number
+                    if destination_column - origin_column > 0:
                         check_direction = 'up'
                     else:
                         check_direction = 'down'
 
-                    if (self.board.columns[int(destination_column) - 1].color_status == self.player.color or self.board.columns[int(destination_column) - 1].color_status == "None") and (self.player.direction == check_direction):
-                        self.board.columns[int(destination_column) - 1].num_pieces += 1
-                        self.board.columns[int(destination_column) - 1].color_status = self.player.color
+                    if (self.board.columns[destination_column - 1].color_status == self.player.color or self.board.columns[destination_column - 1].color_status == "None") and (self.player.direction == check_direction):
+                        self.board.columns[destination_column - 1].num_pieces += 1
+                        self.board.columns[destination_column - 1].color_status = self.player.color
                         self.maneuver_pieces(origin_column)
 
                         valid_destination = True
 
-                    elif (self.board.columns[int(destination_column) - 1].color_status != self.player.color) and (self.board.columns[int(destination_column) - 1].occupied == False) and (self.player.direction == check_direction):
-                        self.board.columns[int(destination_column) - 1].color_status = self.player.color
+                    elif (self.board.columns[destination_column - 1].color_status != self.player.color) and (self.board.columns[destination_column - 1].occupied == False) and (self.player.direction == check_direction):
+                        self.board.columns[destination_column - 1].color_status = self.player.color
                         self.maneuver_pieces(origin_column)
 
                         Move.jail.append([player.color for player in Player.player_list if player.color != self.player.color][0])
@@ -192,17 +173,15 @@ class Move:
                         valid_destination = True
 
                     else:
-                        destination_column = input('Please enter valid destination column: ')
+                        destination_column = self.get_column_number('destination', valid_input2) 
                 else:
-                    destination_column = input('Please enter valid destination column: ')
+                    destination_column = self.get_column_number('destination', valid_input2) 
 
             if destination_column == 'off':
                 self.dice.remove(dice_input)
             else:
-                self.dice.remove(abs(int(destination_column) - int(origin_column)))
-            self.clear()
-            self.board.columns_ascii = []
-            self.board.create_board()
+                self.dice.remove(abs(destination_column - origin_column))
+            self.recreate_board()
             turn_count += 1
 
 
@@ -244,9 +223,9 @@ class Move:
         return any(truth_values)
     
     def maneuver_pieces(self, origin_column):
-        self.board.columns[int(origin_column) - 1].num_pieces -= 1
-        if self.board.columns[int(origin_column) - 1].num_pieces == 0:
-            self.board.columns[int(origin_column) - 1].color_status = "None"
+        self.board.columns[origin_column - 1].num_pieces -= 1
+        if self.board.columns[origin_column - 1].num_pieces == 0:
+            self.board.columns[origin_column - 1].color_status = "None"
         for i in range(24):
             self.board.columns[i].set_occupied()
 
@@ -271,5 +250,26 @@ class Move:
                     total += self.board.columns[23 - i].num_pieces
             if total == 0:
                 return True
-            
 
+    def recreate_board(self):
+        self.clear()
+        self.board.columns_ascii = []
+        self.board.create_board()
+
+    def get_column_number(self, type_column, valid_input, first = False):
+        if first:
+            if type_column == 'origin':
+                column_number = input('Which column would you like to move from? ')
+            elif type_column == 'destination':
+                column_number = input('Which column would you like to move to? ')
+        else: 
+            column_number = input(f'Please enter valid {type_column} column: ')
+
+        while column_number not in valid_input:
+            column_number = input(f'Please enter valid {type_column} column: ') 
+
+        try:
+            int(column_number)
+            return int(column_number)
+        except:
+            return column_number
